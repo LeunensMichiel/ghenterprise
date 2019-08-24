1faf7c5bfbd9ab676e0c637a863d95c1b3de069a
Nicolaas Leenkengt <nic.leenknegt@gmail.com>
2019-08-24 14:13:28 +0200

    Completed enterprise creation

--- Ghenterprise/Models/Street.cs
+++ Ghenterprise/Models/Street.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Ghenterprise.Models
 {
-    public class Street
+    public class Street:ObservableObject
     {
         public string Id { get; set; }
 
