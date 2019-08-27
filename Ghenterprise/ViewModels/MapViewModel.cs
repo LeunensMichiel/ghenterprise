13098f1c6a37cb1fe3a77403d9b0a65eca9e2589
Michiel Leunens <michiel.leunens@gmail.com>
2019-08-28 01:50:04 +0200

    Map works

--- Ghenterprise/ViewModels/MapViewModel.cs
+++ Ghenterprise/ViewModels/MapViewModel.cs
@@ -1,12 +1,16 @@
 ﻿using GalaSoft.MvvmLight;
+using Ghenterprise.Data;
+using Ghenterprise.Models;
 using Ghenterprise.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Devices.Geolocation;
 using Windows.Foundation;
+using Windows.Services.Maps;
 using Windows.Storage.Streams;
 using Windows.UI.Xaml.Controls.Maps;
 
@@ -15,6 +19,9 @@ namespace Ghenterprise.ViewModels
     public class MapViewModel : ViewModelBase
     {
         private const double DefaultZoomLevel = 14;
+        public ObservableCollection<Enterprise> Source { get; } = new ObservableCollection<Enterprise>();
+        private EnterpriseService entService = new EnterpriseService();
+
 
         private readonly BasicGeoposition _ghentposition = new BasicGeoposition()
         {
@@ -50,22 +57,35 @@ namespace Ghenterprise.ViewModels
             {
                 var resources = new Windows.ApplicationModel.Resources.ResourceLoader("api");
                 map.MapServiceToken = resources.GetString("MapServiceToken");
-                AddMapIcon(map, Center, "Gent");
+                var entlist = await entService.GetEnterprisesAsync();
+                entlist.ForEach(async (entie) => {
+                    string addressString = $"{entie.Location.Street.Name} {entie.Location.Street_Number}, 9000 Ghent";
+                    MapLocationFinderResult result = await MapLocationFinder.FindLocationsAsync(addressString, Center, 1);
+
+                    if (result.Status == MapLocationFinderStatus.Success)
+                    {
+                        BasicGeoposition _ghenterpriseLocation = new BasicGeoposition()
+                        {
+                            Latitude = result.Locations[0].Point.Position.Latitude,
+                            Longitude = result.Locations[0].Point.Position.Longitude
+                        };
+                        AddMapIcon(map, new Geopoint(_ghenterpriseLocation), entie.Name);
+                    }
+                });
+
             }
-
         }
-
-        private void AddMapIcon(MapControl map, Geopoint position, string title)
-        {
-            var mapIcon = new MapIcon()
+            private void AddMapIcon(MapControl map, Geopoint position, string title)
             {
-                Location = position,
-                NormalizedAnchorPoint = new Point(0.5, 1.0),
-                Title = title,
-                ZIndex = 0
-            };
-            map.MapElements.Add(mapIcon);
-        }
+                var mapIcon = new MapIcon()
+                {
+                    Location = position,
+                    NormalizedAnchorPoint = new Point(0.5, 1.0),
+                    Title = title,
+                    ZIndex = 0
+                };
+                map.MapElements.Add(mapIcon);
+            }
 
         public override void Cleanup()
         {
